f8d9a81cdfcf303655e64632a06f69bbb260b597
JuanEss7 <juanplazas28pa@gmail.com>
2025-02-03 14:42:35 -0500

    Implementando nuevo contexto AuthListener

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,20 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import ContextProvider from './context/context.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Register from './components/register/Register.tsx'
 import NickName from './components/NickName/NickName.tsx'
 import Login from './components/Login/Login.tsx'
 import Home from './components/home/Home.tsx'
+import AuthListenerProvider from './auth/AuthListener.tsx'
+import './index.css'
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <ContextProvider>
+  <AuthListenerProvider>
       <Routes>
         <Route path='/' element={<Register />}></Route>
         <Route path='/login' element={<Login />}></Route>
-        <Route path='/nickname' element={<NickName />}></Route>
-        <Route path='/home/:nick' element={<Home />}></Route>
+        {/* <Route path='/nickname' element={<NickName />}></Route> */}
+        {/* <Route path='/home/:nick' element={<Home />}></Route> */}
       </Routes>
-    </ContextProvider>
+    </AuthListenerProvider>
   </BrowserRouter>
 )
