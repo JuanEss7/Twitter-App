b3b1d9a04cee6d7c1ea36fd72bf2112abc96030f
JuanEss7 <juanplazas28pa@gmail.com>
2025-02-04 18:33:08 -0500

    Comentando y cambiando tupos en la funcion-action updateInfoTweet

--- src/actions/user/updateInfoTweet.ts
+++ src/actions/user/updateInfoTweet.ts
@@ -1,23 +1,25 @@
 import { Tweet } from "../../interfaces/tweet";
-import { User } from "../../interfaces/user";
 import { updateInfoTweetInFb } from "../db/tweets/updateInfoTweet";
 interface Props {
     tweet: Tweet,
     action: 'like' | 'retweet',
-    user: User
+    userId:string
 }
-export async function updateInfoTweet({ tweet, action, user }: Props) {
+//Funcion que actualiza la informacion en likes y retweet de un tweet en la firebase store
+export async function updateInfoTweet({ tweet, action, userId }: Props) {
     if (action === 'like') {
-        const likesArray = !tweet.like.includes(user.uid) ? [...tweet.like, user.uid] : tweet.like.filter(id => id !== user.uid)
+        //Si no incluye la id del usurio la agrega sino, la elimina
+        const likesArray = !tweet.like.includes(userId) ? [...tweet.like, userId] : tweet.like.filter(id => id !== userId)
         const newTweet: Tweet = { ...tweet, like: likesArray };
         const response = await updateInfoTweetInFb({ tweet: newTweet });
         if (!response) {
             return { ok: false, newTweet: null }
         }
         return { ok: true, newTweet }
-    } else if (action === 'retweet' && tweet.uid !== user.uid) {
-        const retweetsArray = !tweet.retweet.includes(user.uid) ? [...tweet.retweet, user.uid] :
-            tweet.retweet.filter(id => id !== user.uid)
+    } else if (action === 'retweet' && tweet.uid !== userId) {
+         //Si no incluye la id del usurio la agrega sino, la elimina
+        const retweetsArray = !tweet.retweet.includes(userId) ? [...tweet.retweet, userId] :
+            tweet.retweet.filter(id => id !== userId)
         const newTweet: Tweet = { ...tweet, retweet: retweetsArray };
         const response = await updateInfoTweetInFb({ tweet: newTweet })
         if (!response) {
